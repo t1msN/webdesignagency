6680fc5bd10ca66e0e87e67fb0a983c6b1648bc5
t1msn <tim_stehle@posteo.de>
2018-05-03 18:49:20 +0200

    initialcommit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+
+    <!-- css -->
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <!-- javascript & jquery -->
+    <!-- fonts -->
+  </head>
+
+
+
+
+  <body>
+<!-- navbar -->
+
+
+  </body>
+</html>
